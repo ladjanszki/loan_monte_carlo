adbc72ea8e7384172b710173dedff009a0cc994b
István Ladjánszki <istvan.ladjanszki@gmail.com>
2019-02-22 18:08:47 +0100

    * Animation looks good now

--- loanmc.py
+++ loanmc.py
@@ -12,23 +12,24 @@ import numpy as np
 
 # Loading data into DataHandler
 dh = loanmc.DataHandler()
-dh.read_csv('./historical_data/ZERO.csv')
+dh.read_csv('./historical_data/TEST.csv')
+# dh.read_csv('./historical_data/ZERO.csv')
 
 # Fitting the model for all row
 params_df = loanmc.fit_yield_curves(dh.df)
 
-# # Original data in an animation
-# loanmc.plot_yield_curves(dh.df, params_df)
+# Original data in an animation
+loanmc.plot_yield_curves(dh.df, params_df)
 
 # Testing the payment object
 #yc = loanmc.YieldCurve(1, 2, 3, 0.1)
 
-for rows in params_df.iterriws():
-
-    yc = loanmc.YieldCurve(1, 2, 3, 0.1)
-
-    pay = loanmc.Payment(1000000, 120, yc) 
-    print(pay.calc_payment())
+#for rows in params_df.iterrows():
+#
+#    yc = loanmc.YieldCurve(1, 2, 3, 0.1)
+#
+#    pay = loanmc.Payment(1000000, 120, yc) 
+#    print(pay.calc_payment())
 
 
 
--- loanmc/animation.py
+++ loanmc/animation.py
@@ -5,19 +5,21 @@ import matplotlib.animation as animation
 from scipy import optimize
 
 import loanmc
+
  
 def plot_yield_curves(data_df, params_df):
     data_group = data_df.groupby('Date')
-    #params_group = data_df.groupby('Date')
 
     # The figure object
-    fig = plt.figure()
+    fig, ax = plt.subplots()
     
     # All the plots
     ims = []
     
     # DataFrame to store the fit parameteres
     # params_df = pd.DataFrame(columns = ['Date', 'Beta1', 'Beta2', 'Beta3', 'Lambda', 'RSquared'])
+
+    params_df = params_df.set_index('Date')
     
     for key, day in data_group:
 
@@ -26,26 +28,36 @@ def plot_yield_curves(data_df, params_df):
         y_data = day['Yield'].as_matrix()
         last_month = x_data.max()
 
-        # Adding title to the animation
-        # plt.title(key)
-        
         # Plot the original data 
-        orig_data, = plt.plot(x_data, y_data, 'r-.')
+        orig_data, = ax.plot(x_data, y_data, 'r-.', label='Actual data')
+
+        title = ax.text(0.5,
+                        1.05,
+                        "Hungarian zero coupon yield at " + key, 
+                        size=plt.rcParams["axes.titlesize"],
+                        ha="center", 
+                        transform=ax.transAxes, )
         
-        # Plot the fitted data
-        # TODO: This is UGLY for the vectorzied function numpy vectorize have to be explicitly called
-        #       Vectorization have to be done in the class definition
-        params = params_df.loc[params_df['Date'] == key]
+        beta1 = params_df.loc[key]['Beta1']
+        beta2 = params_df.loc[key]['Beta2']
+        beta3 = params_df.loc[key]['Beta3']
+        lmbd = params_df.loc[key]['Lambda']
+
+        ax.set(xlabel = 'Month')
+        ax.set(ylabel = 'Yield [%]')
+
         t = np.linspace(0, last_month, 100)
-        yc = loanmc.YieldCurve(params['Beta1'], params['Beta2'], params['Beta3'], params['Lambda'])
-        func = np.vectorize(yc.calc)
-        fitted, = plt.plot(t, func(t), 'b-')
-        
+        yc = loanmc.YieldCurve(beta1, beta2, beta3, lmbd)
+        tmp2 = yc.calc(t)
+        fitted, = ax.plot(t, tmp2, 'b-', label='Fitted')
+
+        ax.legend()
+
         # Accumulate plots for animation
-        ims.append([orig_data, fitted])
+        ims.append([orig_data, fitted, title])
         
-        
+
     # Creating an animation and save it
-    ani = animation.ArtistAnimation(fig, ims, interval=50, blit=True, repeat_delay=1000)
+    ani = animation.ArtistAnimation(fig, ims, interval=50, blit=False, repeat_delay=1000)
     # ani.save('dynamic_images.mp4')
     plt.show()   
